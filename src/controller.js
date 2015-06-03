2e0294279583e5df90806d4e3fa8c2e88e402433
Nathan John Keightley Sinclair <njs236@student.cpit.ac.nz>
2015-06-04 09:58:24 +1200

    testing random words
    testing, array methods

--- src/controller.js
+++ src/controller.js
@@ -2,13 +2,32 @@ mla.controller = (function (){
     var initModule, Controller, selectedWord, newVocabulary, splicedVocabulary, newTable;
     splicedVocabulary = [];
     
-    Controller = function (view,model) {
-        this.view = view;
-        this.model = model;
+    Controller = function (view1, model1) {
+       var view = view1;
+        console.log(view);
+        var model = model1;
+        console.log(model);
         this.returnModel = function () {
-            return this.model;
-        }
+            return model;
+        };
+        this.display = function () {
+            var items = model.getMyVocabsForDisplay();
+            view.writeSelectTable(items);
+        };
+        
+        this.selectGame = function (id) {
+        /* v1.1 Prepares the view for the game and gives information to the view.
+        */
+        view.viewGameScreen(function () {
+        var vocab = this.model.findVocab(id), word;
+        this.loadVocabulary(vocab);
+        console.log(newVocabulary);
+        word = this.randomSelectionOfWord();
+        var array = [word, ]
+        view.displayWord();
+        })
     }
+    };
     
     
     /**********************************
@@ -16,23 +35,7 @@ mla.controller = (function (){
     Select Screen
     
     **********************************/
-    
-    Controller.prototype.viewLevelSelectScreen = function () {
-        this.view.viewLevelSelectScreen(function () {
-            this.model.getMyVocabsForDisplay()
-        })
- }
-    
-    Controller.prototype.selectGame = function (id) {
-        /* v1.1 Prepares the view for the game and gives information to the view.
-        */
-        this.view.viewGameScreen(function () {
-        var vocab = this.model.findVocab(id), word;
-        this.loadVocabulary(vocab);
-        console.log(newVocabulary);
-        word = this.randomSelectionOfWord();
-        })
-    }
+
     
     /******************************
     
@@ -52,7 +55,7 @@ mla.controller = (function (){
     }
     
     Controller.prototype.randomSelectionOfWord = function () {
-        var rand = Math.floor(Math.random())*splicedVocabulary.length;
+        var rand = Math.floor(Math.random()*(splicedVocabulary.length-1));
         selectedWord = splicedVocabulary[rand];
 
         splicedVocabulary.splice(rand, 1);
@@ -60,8 +63,40 @@ mla.controller = (function (){
         
     }
     
-    Controller.prototype.loadTable = function () {
+    Controller.prototype.loadTable = function (numberOfOptions) {
+        // selects word at random from Vocabulary list
+        // checks randArray for same numbers
+        console.log(this);
+       var random = 0,
+       array = [],
+        randArray = [],
+       i = 0, arrayIndexAlreadyUsed = [];
+       // Populate array with selectedWord so it doesn't get used twice. 
+       randArray.push(newVocabulary.indexOf(selectedWord))
+       arrayIndexAlreadyUsed.push(newVocabulary.indexOf(selectedWord))
+       
+       // every time that a number is selected at random, it checks already used numbers to determine if they are there or not. randArray is actual numbers, while arrayIndexAlreadyUsed is the numbers you cant pick. 
+      
+        for (i; i < numberOfOptions - 1; i++) {
+            random = Math.floor(Math.random()* (newVocabulary.length-1))
+    while (arrayIndexAlreadyUsed.isValidIndex(random) === true) {
+                random= Math.floor(Math.random()* (newVocabulary.length - 1))
+            }
+            randArray.push(random);
+            arrayIndexAlreadyUsed.push(random);
+        }
         
+        // places words in array for displaying
+        console.log(newVocabulary);
+        console.log(randArray);
+        i =0;
+        array.push(selectedWord);
+    for (i; i< numberOfOptions; i++) {
+        array.push(newVocabulary[randArray[i]]);
+    }
+        // sends data to view;
+        
+        return array;
     }
     
     initModule = function () {
@@ -70,10 +105,12 @@ mla.controller = (function (){
         homeScreen: document.getElementById('mainScreen'),
         gameScreen: document.getElementById('gameScreen')});
         var model = mla.model.initModule();
-        var controller = new Controller(view,model)
+        var controller = new Controller(view,model);
         view.setController(controller);
         view.viewScreenListeners();
-        return controller;  
+        controller.display();
+        return controller; 
+        
     }
     
     return {initModule:initModule};
--- src/view.js
+++ src/view.js
@@ -22,10 +22,13 @@ mla.view = (function () {
             screens[i].hidden = true;
         }
     }
+    
+    View.prototype.displayLevelSelectScreen = function () {
+        aView.displayScreen(aView.elements.levelScreen);
+    }
 
-View.prototype.viewLevelSelectScreen = function (callback) {
-    var items = callback();
-    aView.displayScreen(aView.elements.levelScreen);
+View.prototype.writeSelectTable = function (items) {
+    
     var i = 0;
     var div;
     var selectScreenDiv = document.getElementById('selectScreenDiv');
@@ -43,7 +46,9 @@ View.prototype.viewLevelSelectScreen = function (callback) {
         div = document.createElement('DIV');
         div.textContent = items[i];
         div.className = 'selectOptionsButton';
-        div.addEventListener("click", this.controller.selectGame(items[i]));
+        div.addEventListener("click",function () {
+         aView.controller.selectGame(items[i])  
+        });
         selectScreenDiv.lastChild.appendChild(div);
     };
     /* The Back Button */
@@ -86,7 +91,7 @@ View.prototype.viewScreenListeners = function () {
     var playGame = document.getElementById('mainPlayGame');
     var viewResults = document.getElementById('mainViewResults');
     var quit = document.getElementById('mainQuit');
-    playGame.addEventListener("click", this.controller.viewLevelSelectScreen);
+    playGame.addEventListener("click", this.displayLevelSelectScreen);
     viewResults.addEventListener("click", this.viewResultsScreen);
     quit.addEventListener("click", this.quitGame);
     
@@ -104,6 +109,32 @@ Select Screen
 
 *************************/
 
+
+
+
+/*************************
+
+Game Screen
+
+*************************/
+
+View.prototype.displayTextQuestion = function (array) {
+    /*Array:
+    [0] = Word for display;
+    [1] = Words chosen at Random;
+    [1][0-3] = Words chosen at random;
+    */
+    var i = 0;
+    var question = document.getElementById('gameScreenWord');
+    
+    var gameOptions = document.getElementsByClassName('gameOption');
+    
+    question.textContent = array[0];
+    
+    for (i; i < gameOptions.length; i++) {
+        gameOptions[i].textContent = array[1][i];
+    };
+}
 
     initModule = function (elements) {
         aView = new View(elements);
